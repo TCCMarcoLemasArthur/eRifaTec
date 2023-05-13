fdbd66a6b45b7ca670f0551da75eb93dbcdc2714
Larksel <lemuelgames@outlook.com>
2023-05-12 22:25:52 -0300

    Criada estrutura basica com React

---
+++ src/frontend/paginas/App.jsx
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import '../estilos/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello World</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
