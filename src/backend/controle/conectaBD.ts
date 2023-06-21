eeb086d982c1ce7c905b47ac703800f0fbed6b35
marcoantonio-dev <107264950+marcoantonio-dev@users.noreply.github.com>
2023-06-21 13:53:06 -0300

    Update and rename ConectaBD.ts to conectaBD.ts

--- src/backend/controle/conectaBD.ts
+++ src/backend/controle/conectaBD.ts
@@ -1,28 +1,37 @@
-const { Pool } = require('pg');
+import { Pool } from 'pg'
 //Pool é a classe dentro do pacote 'pg' que representa o pool de conexão com o Bd
 
+async function conectaBanco() {
+    const config = {
+        user: 'postgres',
+        password: '2019setembro*',
+        host: '127.0.0.1',
+        port: 5432,
+        database: 'eRifaTecTeste'
+    };
+    
+    const pool = new Pool(config);
 
-const config = {
-    user: 'postgres',
-    password: '2019setembro*',
-    host: '127.0.0.1',
-    port: 5432,
-    database: 'eRifaTecTeste'
-};
-
-
-
-const pool = new Pool(config);
-
-async function validarConexao() {
     try {
         // Obtém uma conexão do pool
-        const client = await pool.connect();
+        const conexao = await pool.connect();
 
         console.log('Conexão estabelecida com sucesso!');
+        return conexao
 
-        // Realize qualquer operação adicional aqui, se necessário
+        /*
+        // Libera a conexão do pool
+        conn.release();
 
+        // Encerra a conexão do pool
+        await pool.end();
+        */
+    } catch (error) {
+        console.error('Erro ao conectar ao banco de dados:', error);
+    }
+}
+
+/*
         // Insere dados em uma tabela
         const idUsuario1 = 2;
         const nomeUsuario1 = "Marco Antônio";
@@ -41,17 +50,5 @@ async function validarConexao() {
         const values1 = [idUsuario1, nomeUsuario1, cpfUsuario1, emailUsuario1, senhaUsuario1, dataNascUsuario1,
             cep1, cidade1, rua1, estado1, bairro1, numero1];
         await client.query(Usuario1, values1);
-
         console.log('Dados inseridos com sucesso!');
-
-        // Libera a conexão do pool
-        client.release();
-
-        // Encerra a conexão do pool
-        await pool.end();
-    } catch (error) {
-        console.error('Erro ao conectar ao banco de dados:', error);
-    }
-}
-
-validarConexao();
+        */
