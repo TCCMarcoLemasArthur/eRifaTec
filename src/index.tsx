4b1269972937d099982d269ba5c9949adf0d32fe
TCCMarcoLemasArthur <132694092+TCCMarcoLemasArthur@users.noreply.github.com>
2023-08-04 14:16:07 -0300

    Correções (1/4)
    - Css reset do Material UI adicionado
    - Rota para o criar rifa adicionada
    - Cores alteradas

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Rotas from './Rotas';
+import CssBaseline from '@mui/material/CssBaseline';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <Rotas />
   </React.StrictMode>
 );
