eb0705a232e31c63255e87bdf7ae8e7dc1957e19
TCCMarcoLemasArthur <132694092+TCCMarcoLemasArthur@users.noreply.github.com>
2023-08-04 14:16:44 -0300

    Correções (2/4)
    - Criar Rifa adicionado

---
+++ src/frontend/paginas/CriarRifa.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from "../componentes/Header";
+import Footer from "../componentes/Footer";
+import FormRifa from '../componentes/FormRifa'
+
+export default function CriarRifa() {
+    return (
+        <>
+            <Header />
+            <FormRifa />
+            <Footer />
+        </>
+    );
+}
