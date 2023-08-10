60333b465341a51b62f63f80317db2cbab648f9f
Larksel <lemuelgames@outlook.com>
2023-08-10 19:38:20 -0300

    Inicializador criado

---
+++ initializer.bat
@@ -0,0 +1,10 @@
+echo off
+echo =====================================================
+echo                Inicializando eRifaTec
+echo =====================================================
+
+npm install -g yarn
+
+yarn install
+
+yarn start
