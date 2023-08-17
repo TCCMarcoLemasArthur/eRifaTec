b561b1b0979de5ca658916be2cc32f12af0f03af
TCCMarcoLemasArthur <132694092+TCCMarcoLemasArthur@users.noreply.github.com>
2023-08-17 14:17:01 -0300

    Update server.ts

--- src/backend/controle/server.ts
+++ src/backend/controle/server.ts
@@ -5,13 +5,20 @@ import path from 'path'
 const app = express()
 const port = 5000
 
+//imports dos métodos
+import cadastrar from './cadastrarUsuario.ts'
+
 // TODO executar o codigo do prisma por aqui
+
+// ! Mover pasta backend para a pasta principal
 
 // server.ts será responsável pela inicialização do servidor e o carregamento de todos os recursos necessários
 // pode conter testes para verificar se a conexão foi bem sucedida
 
 // routes.ts será responsável por designar funções para determinadas rotas
 // quando uma rota for chamada por algum método HTTP (GET, POST, etc), ela executará a função designada para aquela rota
+
+app.use
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -20,3 +27,11 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
+
+
+//Mover para a Pasta Rotas
+app.post('/cadastrar', (req, res) => {
+    //cadastrar();
+    //res.send("");
+    console.log("Cadastro Realizado com Sucesso");
+}); 
