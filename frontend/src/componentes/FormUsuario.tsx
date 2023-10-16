208bb3a114528700944d4e6838c2fedead183b7f
Larksel <lemuelgames@outlook.com>
2023-10-16 00:12:59 -0300

    FormUsuario em processo de migração p/ MaterialUI

--- frontend/src/componentes/FormUsuario.tsx
+++ frontend/src/componentes/FormUsuario.tsx
@@ -1,9 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import '../estilos/formusuario.css';
 import "../estilos/campotexto.css";
-import CampoTexto from './CampoTexto';
-import { Link } from 'react-router-dom'
-import axios from 'axios'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+import Grid from '@mui/material/Unstable_Grid2';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button'
 
 // TODO Preparar os campos para os dados que serão inseridos (máscara no campo dataDeNascimento, filtro e verificação)
 
@@ -29,7 +33,7 @@ function FormUsuario({cadastro}: Props) {
     rua: '',
     numero: ''
   })
-
+  const [confirmacao, setConfirmacao] = useState(true)
   const [response, setResponse] = useState('')
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -37,7 +41,7 @@ function FormUsuario({cadastro}: Props) {
     setUserData({ ...userData, [name]: value });
   };
 
-   //! Verificar a confirmação da senha, verificar campos e procurar uma alternativa ao useState
+   //! Verificar campos e procurar uma alternativa ao useState
 
   const handleCadastrar = (event: React.FormEvent) => {
     // Impede a página de recarregar quando clicar no botão
@@ -62,7 +66,6 @@ function FormUsuario({cadastro}: Props) {
       .catch(erro => {
         console.log('Erro ao fazer postagem', erro)
       })
-  
   }
   
   const handleAtualizar = (event: React.FormEvent) => {
@@ -70,179 +73,247 @@ function FormUsuario({cadastro}: Props) {
     // TODO implementar método de atualização
   }
 
+  const confirmarSenha = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    if (value === userData.senha && (value !== '' && userData.senha !== '')) {
+      //console.log('senha confirmada')
+      setConfirmacao(true);
+    } else {
+      //console.log('senha não confirmada')
+      setConfirmacao(false);
+    }
+  }
+
+  const formStyles = {
+    /*
+    display: 'flex',
+    flexDirection: 'column', */
+    margin: '15px auto',
+    width: '100%',
+    maxWidth: '46rem',
+    backgroundColor: 'white',
+    padding: '15px 20px 0',
+    borderRadius: '10px',
+    boxShadow: '0 0 17px rgba(0, 0, 0, 0.3)'
+  }
+
+  const textFieldStyles = {
+    input: {color: '#515151'},
+    label: {color: '#7f858d'},
+    fieldSet: {borderColor: '#7f858d'},
+    width: '100%'
+  }
+
   return (
-    <>
-      <form onSubmit={cadastro ? handleCadastrar : handleAtualizar} className="formUsuario">
-        <h2 className="titulo">{cadastro ? "Cadastro" : "Meus dados"}</h2>
+    <Grid 
+      container 
+      component="form" 
+      onSubmit={cadastro ? handleCadastrar : handleAtualizar} 
+      xs={12} 
+      sx={formStyles}
+    >
+      <Typography variant='h2' component='h2' sx={{
+        color: 'primary.main',
+        textAlign: 'center',
+        width: '100%',
+        fontSize: '2rem',
+        fontWeight: 'bold',
+        marginBottom: '15px'
+      }}>
+        {cadastro ? 'Cadastro' : 'Meus dados'}
+      </Typography>
 
-        <div className="grupo-campo">
-          <div className="label-campo">
-            <input
-              type='text'
-              name='nome'
-              id='nome'
-              value={userData.nome}
-              required
-              onChange={handleInputChange}
-            />
-            <label>Nome*</label>
-          </div>
-          <div className="label-campo">
-            <input
-              type='text'
-              name='cpf'
-              id='cpf'
-              value={userData.cpf}
-              required
-              onChange={handleInputChange}
-            />
-            <label>cpf*</label>
-          </div>
-        </div>
+      <Grid xs={12} md={6}>
+        <TextField
+          type='text'
+          id='nome'
+          name='nome'
+          label='Nome'
+          variant='outlined'
+          value={userData.nome}
+          onChange={handleInputChange}
+          required
+          sx={textFieldStyles}
+        />
+      </Grid>
+      <Grid xs={12} md={6}>
+        <TextField
+          type='text'
+          id='cpf'
+          name='cpf'
+          label='CPF'
+          variant='outlined'
+          value={userData.cpf}
+          onChange={handleInputChange}
+          required
+          sx={textFieldStyles}
+        />
+      </Grid>
 
-        <div className="grupo-campo">
-          <div className="label-campo">
-              <input
-                type='text'
-                name='dataNasc'
-                id='dataNasc'
-                value={userData.dataNasc}
-                required
-                onChange={handleInputChange}
-              />
-              <label>dataNasc*</label>
-            </div>
-            <div className="label-campo">
-              <input
-                type='text'
-                name='celular'
-                id='celular'
-                value={userData.celular}
-                required
-                onChange={handleInputChange}
-              />
-              <label>celular*</label>
-            </div>
-        </div>
+      <Grid xs={12} md={6}>
+        <TextField
+          type='date'
+          id='dataNasc'
+          name='dataNasc'
+          label='Data de Nascimento'
+          variant='outlined'
+          value={userData.dataNasc}
+          onChange={handleInputChange}
+          required
+          sx={textFieldStyles}
+        />
+      </Grid>
+      <Grid xs={12} md={6}>
+        <TextField
+          type='text'
+          id='celular'
+          name='celular'
+          label='Celular'
+          variant='outlined'
+          value={userData.celular}
+          onChange={handleInputChange}
+          required
+          sx={textFieldStyles}
+        />
+      </Grid>
 
-        <div className="grupo-campo">
-          <div className="label-campo">
-            <input
-              type='text'
-              name='email'
-              id='email'
-              value={userData.email}
-              required
-              onChange={handleInputChange}
-            />
-            <label>email*</label>
-          </div>
-        </div>
+      <Grid xs={12}>
+        <TextField
+          type='text'
+          id='email'
+          name='email'
+          label='Email'
+          variant='outlined'
+          value={userData.email}
+          onChange={handleInputChange}
+          required
+          sx={textFieldStyles}
+        />
+      </Grid>
 
-        <div className="grupo-campo">
-          <div className="label-campo">
-            <input
-              type='password'
-              name='senha'
-              id='senha'
-              value={userData.senha}
-              required
-              onChange={handleInputChange}
-            />
-            <label>senha*</label>
-          </div>
-          <div className="label-campo">
-            <input
-              type='password'
-              name='confirmacaosenha'
-              id='confirmacaosenha'
-              required
-            />
-            <label>confirmacaosenha*</label>
-          </div>
-        </div>
+      <Grid xs={12} md={6}>
+        <TextField
+          type='password'
+          id='senha'
+          name='senha'
+          label='Senha'
+          variant='outlined'
+          value={userData.senha}
+          onChange={handleInputChange}
+          required
+          sx={textFieldStyles}
+        />
+      </Grid>
+      <Grid xs={12} md={6}>
+        <TextField
+          type='password'
+          id='confirmacaosenha'
+          name='confirmacaosenha'
+          label='Confirme sua senha'
+          variant='outlined'
+          onChange={confirmarSenha}
+          error={!confirmacao}
+          required
+          sx={textFieldStyles}
+        />
+      </Grid>
 
-        <div className="grupo-campo">
-        <div className="label-campo">
-            <input
-              type='text'
-              name='cep'
-              id='cep'
-              value={userData.cep}
-              required
-              onChange={handleInputChange}
-            />
-            <label>cep*</label>
-          </div>
-          <div className="label-campo">
-            <input
-              type='text'
-              name='estado'
-              id='estado'
-              value={userData.estado}
-              required
-              onChange={handleInputChange}
-            />
-            <label>estado*</label>
-          </div>
-          <div className="label-campo">
-            <input
-              type='text'
-              name='cidade'
-              id='cidade'
-              value={userData.cidade}
-              required
-              onChange={handleInputChange}
-            />
-            <label>cidade*</label>
-          </div>
-        </div>
+      <Grid xs={12} md={4}>
+        <TextField
+          type='text'
+          id='cep'
+          name='cep'
+          label='CEP'
+          variant='outlined'
+          value={userData.cep}
+          onChange={handleInputChange}
+          required
+          sx={textFieldStyles}
+        />
+      </Grid>
+      <Grid xs={12} md={4}>
+        <TextField
+          type='text'
+          id='estado'
+          name='estado'
+          label='Estado'
+          variant='outlined'
+          value={userData.estado}
+          onChange={handleInputChange}
+          required
+          sx={textFieldStyles}
+        />
+      </Grid>
+      <Grid xs={12} md={4}>
+        <TextField
+          type='text'
+          id='cidade'
+          name='cidade'
+          label='Cidade'
+          variant='outlined'
+          value={userData.cidade}
+          onChange={handleInputChange}
+          required
+          sx={textFieldStyles}
+        />
+      </Grid>
 
-        <div className="grupo-campo">
-        <div className="label-campo">
-            <input
-              type='text'
-              name='bairro'
-              id='bairro'
-              value={userData.bairro}
-              required
-              onChange={handleInputChange}
-            />
-            <label>bairro*</label>
-          </div>
-          <div className="label-campo">
-            <input
-              type='text'
-              name='rua'
-              id='rua'
-              value={userData.rua}
-              required
-              onChange={handleInputChange}
-            />
-            <label>rua*</label>
-          </div>
-          <div className="label-campo">
-            <input
-              type='text'
-              name='numero'
-              id='numero'
-              value={userData.numero}
-              required
-              onChange={handleInputChange}
-            />
-            <label>numero*</label>
-          </div>
-        </div>
+      <Grid xs={12} md={4}>
+        <TextField
+          type='text'
+          id='bairro'
+          name='bairro'
+          label='Bairro'
+          variant='outlined'
+          value={userData.bairro}
+          onChange={handleInputChange}
+          required
+          sx={textFieldStyles}
+        />
+      </Grid>
+      <Grid xs={12} md={4}>
+        <TextField
+          type='text'
+          id='rua'
+          name='rua'
+          label='Rua'
+          variant='outlined'
+          value={userData.rua}
+          onChange={handleInputChange}
+          required
+          sx={textFieldStyles}
+        />
+      </Grid>
+      <Grid xs={12} md={4}>
+        <TextField
+          type='text'
+          id='numero'
+          name='numero'
+          label='Número'
+          variant='outlined'
+          value={userData.numero}
+          onChange={handleInputChange}
+          required
+          sx={textFieldStyles}
+        />
+      </Grid>
 
-        <div className="button-box">
-          <button type="submit" id={cadastro ? "btnCadastrar" : "btnAtualizar"}>
-            Enviar
-          </button>
-          {cadastro ? <p><span style={{color: 'black'}}>Já possui uma conta? </span><Link to={'/login'}>Entrar</Link></p> : ""}
-        </div>
-      </form>
-      <p>{response}</p>
-    </>
+      <Grid xs={12} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <Button type='submit' variant='contained' id={cadastro ? 'btnCadastrar' : 'btnAtualizar'}>
+          Enviar
+        </Button>
+      </Grid>
+      <Grid xs={12} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        {cadastro ? 
+          <Typography component='p' sx={{color: 'black'}}>
+            Já possui uma conta?
+            <Typography component='span' sx={{a: {textDecoration: 'underline', color: 'primary.main'}}}>
+              <Link to={'/login'}> Entre aqui</Link>
+            </Typography>
+          </Typography>
+        : ''}
+      </Grid>
+    </Grid>
   );
 }
 
