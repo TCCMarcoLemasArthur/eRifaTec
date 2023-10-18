7e6e61949a5cb3225d42e0ba835f75a75518a886
Larksel <lemuelgames@outlook.com>
2023-10-17 23:12:44 -0300

    Página listar usuários concluída

--- frontend/src/Rotas.tsx
+++ frontend/src/Rotas.tsx
@@ -5,19 +5,21 @@ import CadastroUsuario from './paginas/CadastroUsuario';
 import LoginUsuario from './paginas/LoginUsuario';
 import CriarRifa from './paginas/CriarRifa'
 import NoPage from './paginas/NoPage';
+import ListarUsuarios from './paginas/ListarUsuarios';
 
 export default function Rotas() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' index element={<Home />}/>
-                    <Route path='/login' element={<LoginUsuario />}/>
-                    <Route path='/cadastrar' element={<CadastroUsuario />}/>
-                    <Route path='/criar_rifa' element={<CriarRifa />}/>
-                    <Route path='*' element={<NoPage />}/>
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' index element={<Home />}/>
+					<Route path='/login' element={<LoginUsuario />}/>
+					<Route path='/cadastrar' element={<CadastroUsuario />}/>
+					<Route path='/criar_rifa' element={<CriarRifa />}/>
+					<Route path='/listar_usuarios' element={<ListarUsuarios />}/>
+					<Route path='*' element={<NoPage />}/>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
--- frontend/src/paginas/Home.tsx
+++ frontend/src/paginas/Home.tsx
@@ -2,12 +2,33 @@ import React from 'react'
 import HeaderUI from "../componentes/HeaderUI";
 import Footer from "../componentes/Footer";
 
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
 
-export default function LoginUsuario() {
-    return (
-        <>
-            <HeaderUI />
-            <Footer />
-        </>
-    );
+import { useNavigate } from 'react-router-dom';
+
+export default function Home() {
+  const navigate = useNavigate()
+
+  const listarUsuario = () => {
+    navigate('/listar_usuarios')
+  }
+
+  return (
+    <>
+      <HeaderUI />
+      <Container sx={{ 
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center', 
+        width: '100%',
+        p: '20px 0'
+      }}>
+        <Button onClick={listarUsuario} variant='contained'>
+          Listar Usuários
+        </Button>
+      </Container>
+      <Footer />
+    </>
+  );
 }
