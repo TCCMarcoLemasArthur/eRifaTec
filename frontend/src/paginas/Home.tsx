bec771c3a933cd1467190143b38f1aa1994b3557
Larksel <lemuelgames@outlook.com>
2023-10-19 23:15:20 -0300

    - Adicionado atalhos para as páginas existentes

--- frontend/src/paginas/Home.tsx
+++ frontend/src/paginas/Home.tsx
@@ -4,14 +4,27 @@ import Footer from "../componentes/Footer";
 
 import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
 
 import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
   const navigate = useNavigate()
 
+  const cadastrarUsuario = () => {
+    navigate('/cadastrar')
+  }
   const listarUsuario = () => {
     navigate('/listar_usuarios')
+  }
+  const loginUsuario = () => {
+    navigate('/login')
+  }
+  const criarRifa = () => {
+    navigate('/criar_rifa')
+  }
+  const noPage = () => {
+    navigate('/awgawg')
   }
 
   return (
@@ -19,13 +32,43 @@ export default function Home() {
       <Header />
       <Container sx={{ 
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center', 
         width: '100%',
         p: '20px 0'
       }}>
-        <Button onClick={listarUsuario} variant='contained'>
+        <Typography variant='h1' component='h1' sx={{
+          fontSize: 34,
+          p: 2,
+          mb: 2,
+          borderRadius: '10px',
+          boxShadow: '0 0 17px #890DB7',
+          borderWidth: '5px',
+          borderStyle: 'double',
+          borderColor: '#890DB7 #B407DB #DF00FF'
+        }}>
+          Páginas existentes no momento
+        </Typography>
+
+        <Button onClick={cadastrarUsuario} variant='contained' sx={{marginBottom: 2}}>
+          Cadastrar Usuário
+        </Button>
+
+        <Button onClick={loginUsuario} variant='contained' sx={{marginBottom: 2}}>
+          Login
+        </Button>
+
+        <Button onClick={listarUsuario} variant='contained' sx={{marginBottom: 2}}>
           Listar Usuários
+        </Button>
+
+        <Button onClick={criarRifa} variant='contained' sx={{marginBottom: 2}}>
+          Criar Rifa
+        </Button>
+
+        <Button onClick={noPage} variant='contained' color='error'>
+          Página 404
         </Button>
       </Container>
       <Footer />
