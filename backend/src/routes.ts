d4d6519be1c1a18a360e46a243b73735efc90fc4
Marco Antônio <maarcoantoniomasson@gmail.com>
2023-10-26 19:49:18 -0300

    rotas, tentando pegar o idUsusario(frontend) para chamar a função de atualizar

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'; 
 
+
 import { criarUsuario, listarUsuario, atualizarUsuario, deletarUsuario, loginUsuario} from "./controller/UserController.js";
 
 import { criarRifa, listarRifa, listarRifaPorId, atualizarRifa, deletarRifa} from "./controller/RifaController.js";
@@ -23,6 +24,16 @@ router.put('/atualizarrifa', atualizarRifa);
 router.delete('/deletarrifa', deletarRifa);
 
 router.get('/consultarcep', consultarCep)
+// Exemplo de como enviar o ID do usuário para o frontend
+router.get('/user/:id', async (req, res) => {
+    const userId = req.params.id; // Suponha que você tenha o ID do usuário
+  
+    // Realize a lógica necessária para obter os dados do usuário com o ID fornecido
+  
+    // Envie o ID do usuário como resposta
+    res.json({ userId: userId });
+  });
+  
 
 export default router;
 
