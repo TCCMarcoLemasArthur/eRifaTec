e6163898b204e44c00b014605e821551d504baee
TCCMarcoLemasArthur <132694092+TCCMarcoLemasArthur@users.noreply.github.com>
2023-10-27 14:21:53 -0300

    Botão delete GridUsuarios.tsx

--- frontend/src/componentes/GridUsuarios.tsx
+++ frontend/src/componentes/GridUsuarios.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import Button from '@mui/material/Button'
+import DeleteIcon from '@mui/icons-material/Delete'
 
 const columns: GridColDef[] = [
   { field: 'idusuario', headerName: 'ID', flex: 1 },
@@ -21,7 +23,7 @@ const columns: GridColDef[] = [
 
 export default function GridUsuarios() {
   const [users, setUsers] = useState([])
-
+  
   useEffect(() => {
     axios.get('http://localhost:5000/listarusuario')
       .then(response => {
@@ -33,20 +35,34 @@ export default function GridUsuarios() {
       })
   }, []);
 
+  const handleDelete = () => {
+    // Código para deletar um ou mais registros
+  }
+
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={users}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
-        }}
-        getRowId={(row: any) => row.idusuario}
-        pageSizeOptions={[5, 10, 15, 20]}
-        checkboxSelection
-      />
-    </div>
+    <>
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={users}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          getRowId={(row: any) => row.idusuario}
+          pageSizeOptions={[5, 10, 15, 20]}
+          checkboxSelection
+        />
+      </div>
+      <Button 
+        variant="outlined" 
+        startIcon={<DeleteIcon />} 
+        onClick={handleDelete}
+        sx={{m: 2}}
+      >
+        Delete
+      </Button>
+    </>
   );
 }
